7f8d89f30bead012e1f97359e0e4744a9937ec79
hyukjun <hyukjun1994@gmail.com>
2021-04-13 15:10:32 +0900

    test2

--- learning/main.tf
+++ learning/main.tf
@@ -86,17 +86,6 @@ resource "azurerm_network_security_group" "nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
-  security_rule {
-    name                       = "HTTP"
-    priority                   = 1003
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "80"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
 }
 
 # Create network interface
