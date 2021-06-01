9164bd22cea4372c80474c05c76b955d98508854
namhj94 <hyukjun1994@gmail.com>
2021-06-01 19:08:30 +0900

    cloud test

--- azure-infra-01/variables.tf
+++ azure-infra-01/variables.tf
@@ -10,10 +10,8 @@ variable "prefix"{
     default = "hol-1"
 }
 variable "admin_username" {
-    default = "azureuser"
     description = "vm admin username"
 }
 variable "admin_password" {
-    default = "dkagh1.dkagh1."
     description = "vm admin password"
 }
